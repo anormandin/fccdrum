13f8d3d2ce4ecc1c6f0e3836ac6409f8a8db49a9
Alain Normandin <4203914+anormandin@users.noreply.github.com>
2020-04-03 13:34:24 -0400

    version 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,40 @@ import React from 'react';
 import { Pad } from "./components/pad/Pad";
 import './App.css';
 
+import kick1 from './Sounds/kick1.wav';
+import kick2 from './Sounds/kick2.wav';
+import snare from './Sounds/snare.wav';
+import chat from './Sounds/chat.wav';
+import cymbal from './Sounds/cymbal.wav';
+import rim from './Sounds/rim.wav';
+import tom from './Sounds/tom.wav';
+import clap from './Sounds/clap.wav';
+
+
 function App() {
+
+  const samples = [
+    { label:'Kick 1', actionKey: 'Q', soundFx: kick1 },
+    { label:'Kick 2', actionKey: 'W', soundFx: kick2 },
+    { label:'Snare', actionKey: 'E', soundFx: snare },
+    { label:'C-Hat', actionKey: 'R', soundFx: chat },
+    { label:'Cymbal', actionKey: 'A', soundFx: cymbal },
+    { label:'Rim', actionKey: 'S', soundFx: rim },
+    { label:'Tom', actionKey: 'D', soundFx: tom },
+    { label:'Clap', actionKey: 'F', soundFx: clap },
+  ]
+ 
   return (
+
     <div className="App">
-        <div className="deck">
-          <Pad label="Kick 1" actionKey="A" />
-          <Pad label="Kick 2" actionKey="S"   />
-          <Pad label="Snare" actionKey="D"   />
-          <Pad label="Clap" actionKey="F"   />
-          <Pad label="Rim" actionKey="G"   />
-          <Pad label="CHat" actionKey="H"   />
-          <Pad label="Tom" actionKey="J"   />
-          <Pad label="Cymbal" actionKey="K"   />
+      <div className="deck">
+        <div className="header">Drum Master 3000</div>
+          <div className="deck-controls">
+            {samples.map(smp => <Pad {...smp} key={smp.actionKey} />)}
+          </div>
         </div>
     </div>
+
   );
 }
 
--- src/components/pad/Pad.js
+++ src/components/pad/Pad.js
@@ -3,10 +3,41 @@ import React from 'react';
 import './pad.css';
 import { KnobCtrl } from '../knob/Knob';
 
+
 export class Pad extends React.Component {
-    
-    
+    constructor(props, state) {
+        super(props, state);
+        
+        this.fx = new Audio(this.props.soundFx);
+        this.state = { active: false };
+    }
+   
+    handleKeyPress = (e) => {
+        if (e.key.toUpperCase() === this.props.actionKey) {
+            this._btn.backgroundColor = 'pink';
+          this.playFx();
+        }
+      }
+
+    playFx = () => {
+        this.setState({active:true});
+        this.fx.currentTime = 0;
+        this.fx.play();
+        setTimeout(() => this.setState({active:false}), 100);
+    }
+
+    componentDidMount = () => {
+        document.addEventListener('keydown', this.handleKeyPress);
+    }
+
+    componentWillUnmount = () => {
+        document.removeEventListener('keydown', this.handleKeyPress);
+    }
+
     render() {
+
+        const className = this.state.active ? "pad-button active" : "pad-button"
+
         return <div className='pad'>
             <div className="pad-label">{this.props.label}</div>
             <div className="pad-knobs">
@@ -14,7 +45,7 @@ export class Pad extends React.Component {
                 <KnobCtrl label="pitch"  />
                 <KnobCtrl label="decay"  />
             </div>
-            <button className="pad-button">{this.props.label}<br />{this.props.actionKey}</button>
+            <button   ref={(btn) => this._btn = btn} className={className} onClick={this.playFx}>{this.props.label}<br />{this.props.actionKey}</button>
         </div>
     }
 }
